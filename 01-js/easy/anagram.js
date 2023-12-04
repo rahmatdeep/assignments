b108c3f3da02efc7fadeae07d225d5c1dbfdbc93
rahmatdeep <rahmatdeep@gmail.com>
2023-12-04 13:30:07 +0530

    anagram solution

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,35 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase()
+  str2 = str2.toLowerCase()
+  console.log(str1, str2)
+  let letters1 = []
+  let letters2 = []
+  for(let i=0; i<str1.length; i++){
+    letters1.push(str1[i])
+  }
+  for(let i=0; i<str2.length; i++){
+    letters2.push(str2[i])
+  }
+  console.log(letters1, letters2)
+  letters1.sort()
+  letters2.sort();
+  console.log(letters1, letters2)
+  if(letters1.length!==letters2.length){
+    return false
+  }
+  else{
+    for(let i=0; i<letters1.length; i++){
+      if(letters1[i]!==letters2[i]){
+        return false
+      }
+    }
+    return true
+}
 
 }
 
 module.exports = isAnagram;
+
+console.log(isAnagram("Red", "dEr"));
