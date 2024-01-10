7c52e2e4b105cfbbc630b01a5cef31279c646ee2
rahmatdeep <rahmatdeep@gmail.com>
2024-01-10 16:00:41 +0530

    Updated formatting using prettier

--- week-4/react-state/src/App.jsx
+++ week-4/react-state/src/App.jsx
@@ -1,77 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-
-let globalId = 4
+let globalId = 4;
 function App() {
-  const [todos, setTodos] = useState([{
-    id: 1,
-    title: "ONE",
-    description: "This is ONE"
-  }, {
-    id: 2,
-    title: "TWO",
-    description: "This is TWO"
-  }, {
-    id: 3,
-    title: "THREE",
-    description: "This is THREE"
-  }])
-const [inputTitle, setInputTitle] = useState("")
-const [inputDescription, setInputDescription] = useState("")
-const addTodo = () => {
-  const newTodo = {
-    id: globalId++,
-    title: inputTitle,
-    description: inputDescription
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "ONE",
+      description: "This is ONE",
+    },
+    {
+      id: 2,
+      title: "TWO",
+      description: "This is TWO",
+    },
+    {
+      id: 3,
+      title: "THREE",
+      description: "This is THREE",
+    },
+  ]);
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputDescription, setInputDescription] = useState("");
+  const addTodo = () => {
+    const newTodo = {
+      id: globalId++,
+      title: inputTitle,
+      description: inputDescription,
+    };
+    setTodos([...todos, newTodo]);
   };
-  setTodos([...todos, newTodo]);
-};
 
-const deleteTodo = (idToDelete) => {
-  const updatedTodos = todos.filter(todo => todo.id !== idToDelete);
-  setTodos(updatedTodos);
-};
+  const deleteTodo = (idToDelete) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== idToDelete);
+    setTodos(updatedTodos);
+  };
 
-  return(
+  return (
     <>
-    <input type='text' onChange={(e)=>setInputTitle(e.target.value)} /><br />
-    <input type="text" onChange={(e)=>setInputDescription(e.target.value)} /><br />
-    <button onClick={addTodo}>Add Todo</button>
-    
-    {todos.map((todo)=>(
-      <div key={todo.id} >
-    <Todo title={todo.title} description={todo.description} />
-    <button onClick={()=>{deleteTodo(todo.id)}}>X</button>
-    </div>
-    ))}
-    
-    
+      <input type="text" onChange={(e) => setInputTitle(e.target.value)} />
+      <br />
+      <input
+        type="text"
+        onChange={(e) => setInputDescription(e.target.value)}
+      />
+      <br />
+      <button onClick={addTodo}>Add Todo</button>
+
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <Todo title={todo.title} description={todo.description} />
+          <button
+            onClick={() => {
+              deleteTodo(todo.id);
+            }}
+          >
+            X
+          </button>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-
-function Todo({title, description}){
-  return(
+function Todo({ title, description }) {
+  return (
     <div>
-      <h1>
-        {title}
-      </h1>
-      <h3>
-        {description}
-      </h3>
-      
+      <h1>{title}</h1>
+      <h3>{description}</h3>
     </div>
-  )
+  );
 }
 
-
-
-
-
-
-
-export default App
+export default App;
